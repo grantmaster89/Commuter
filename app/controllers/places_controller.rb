8d96b1c44197c36649ab6e801bbd3d3ec26da210
Grant Cottle <grantcottle@Grants-MacBook-Pro.local>
2019-05-19 14:24:02 -0700

    addind changes to places controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,11 +1,12 @@
-class PlacesController < ApplicationController
+# frozen_string_literal: true
 
+class PlacesController < ApplicationController
   def index
     @places = Place.all
   end
 
   def new
-    @place = Place.new  
+    @place = Place.new
   end
 
   def show
@@ -15,15 +16,15 @@ class PlacesController < ApplicationController
   def create
     # TODO: if called from trips
     # 1) populate name field
-    # 2) redirect back to trip create
+    # 2) redirect back to trip create or back
     @place = Place.new(place_params)
-    render json: @place.as_json(only: [:name, :street, :city, :state, :zip]) if @place.save
+    render json: @place.as_json(only: %i[name street city state zip]) if @place.save
   end
 
   private
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def place_params
-      params.require(:place).permit(:name, :street, :city, :state, :zip)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def place_params
+    params.require(:place).permit(:name, :street, :city, :state, :zip)
+  end
 end
